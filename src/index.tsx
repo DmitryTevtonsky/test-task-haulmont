80067e0481ebb658d51d8b4dc9353d06d07dc3a7
d.prusakov@innopolis.ru <d.prusakov@innopolis.ru>
2021-06-30 20:42:38 +0300

    Template inited.

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { Core } from "./core";
+
+ReactDOM.render(<Core />, document.getElementById("root"));
---
+++ stylelint.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: [
+    'stylelint-config-recommended',
+    'stylelint-config-css-modules',
+    'stylelint-config-property-sort-order-smacss'
+  ],
+  rules: {}
+};
