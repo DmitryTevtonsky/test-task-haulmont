2bd98a3968ba141c88d89fab7ba9d52621c7fb8e
d.prusakov@innopolis.ru <d.prusakov@innopolis.ru>
2021-07-01 16:16:27 +0300

    Updates

---
+++ precache-manifest.8b064979e714f4394d1e31d70f86ed6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c1bdf69f7863ba8f8d720ceabcd1296",
+    "url": "/test-task-haulmont/index.html"
+  },
+  {
+    "revision": "0536623dd4c8b3de67ce",
+    "url": "/test-task-haulmont/static/css/main.15f6ce2f.chunk.css"
+  },
+  {
+    "revision": "afee1018073894801113",
+    "url": "/test-task-haulmont/static/js/2.1daa4866.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/test-task-haulmont/static/js/2.1daa4866.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0536623dd4c8b3de67ce",
+    "url": "/test-task-haulmont/static/js/main.e06e20f1.chunk.js"
+  },
+  {
+    "revision": "d963b358d27b779c87e1",
+    "url": "/test-task-haulmont/static/js/runtime-main.90d745e2.js"
+  },
+  {
+    "revision": "0536623dd4c8b3de67ce",
+    "url": "/test-task-haulmont/test-task-haulmont\\css\\chunks\\0.15f6ce2fa9d96c3338d9.css"
+  }
+]);
