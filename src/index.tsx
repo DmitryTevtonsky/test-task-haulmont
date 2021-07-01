00c66fa939b1995e63f70b3773f1a605ece0418b
d.prusakov@innopolis.ru <d.prusakov@innopolis.ru>
2021-07-01 15:02:07 +0300

    Updates

--- src/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import { Core } from "./core";
-
-ReactDOM.render(<Core />, document.getElementById("root"));
--- stylelint.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: [
-    'stylelint-config-recommended',
-    'stylelint-config-css-modules',
-    'stylelint-config-property-sort-order-smacss'
-  ],
-  rules: {}
-};
